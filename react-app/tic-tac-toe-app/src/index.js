f13c5cc2ef6af6e9916c522bb516b72adbe58ea5
klpatil <klpatil@hotmail.com>
2021-09-20 23:29:55 -0500

    Before lifting state up to Game level

--- react-app/tic-tac-toe-app/src/index.js
+++ react-app/tic-tac-toe-app/src/index.js
@@ -12,44 +12,17 @@ function Square(props) {
   }
   
   class Board extends React.Component {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext : true,
-        };
-    }
-
-    handleClick(i){
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {      return;    }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares:squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
     renderSquare(i) {
       return (
-        <Square value={this.state.squares[i]}
-                    onClick={() => this.handleClick(i)}
+        <Square value={this.props.squares[i]}
+                    onClick={() => this.props.onClick(i)}
         />
       );
     }
   
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if(winner){
-            status = 'Winner : ' + winner;
-        }else{
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-    
       return (
-        <div>
-          <div className="status">{status}</div>
+        <div>          
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -71,21 +44,111 @@ function Square(props) {
   }
   
   class Game extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        stepNumber:0,
+        xIsNext:true,
+      }
+    }
+
+    handleClick(i){
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if(calculateWinner(squares) || squares[i]){
+        return;
+      }
+
+    
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+          history : history.concat([{
+            squares:squares
+          }]),
+          stepNumber : history.length,
+          xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step){
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
+
+    resetGame(){
+
+      var result = window.confirm("Are you sure you want to reset game?");
+      if(result)
+      {
+        this.setState({
+          history: [{
+            squares: Array(9).fill(null),
+          }],
+          stepNumber:0,
+          xIsNext:true,
+        });
+
+        alert("Game rest completed!");
+     }
+     else
+     {
+       alert("User denied to reset game");
+     }
+    }
+
     render() {
-      return (
-          
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+            return(
+              
+              <li key={move}>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+              </li>
+            )
+      });
+ 
+      // KP Defined
+      const resetGameButton = <button className="resetButton" onClick={() => this.resetGame()}>Reset</button>;
+       
+      let status;
+      if(winner)
+      {
+        status = 'Winner : ' + winner;
+      } 
+      else
+      {
+        status = 'Next player : ' + (this.state.xIsNext? 'X' : 'O');
+      } 
+
+      return (          
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+              squares={current.squares}
+              onClick = {(i) => this.handleClick(i)}
+            />
           </div>
 
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <div>{resetGameButton}</div>
+            <ol>{moves}</ol>
           </div>
           <div>
          
-          <button>Reset Game</button>
+          
          
           </div>
         </div>
